62a6477885dea3bfaf8c93ef5a7fa783f496c05f
pacs <pacs@outlook.pt>
2022-03-31 14:08:08 +0100

    restarted bossmachine

--- bossmachine/server/api.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const apiRouter = express.Router();
-
-
-
-module.exports = apiRouter;
--- bossmachine/server/checkMillionDollarIdea.js
+++
@@ -1,4 +0,0 @@
-const checkMillionDollarIdea = () => {};
-
-// Leave this exports assignment so that the function can be used elsewhere
-module.exports = checkMillionDollarIdea;
