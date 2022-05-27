c3addf5db94896b29441e5cad81b5f935c14d8ad
pacs <pacs@outlook.pt>
2022-05-27 23:29:49 +0100

    just problems with creating the tables

--- Restaurant/src/main/java/com/example/Restaurant/model/Restaurant.java
+++ Restaurant/src/main/java/com/example/Restaurant/model/Restaurant.java
@@ -24,9 +24,6 @@ public class Restaurant {
     @Column(name="Address")
     private String address;
 
-    @Column(name="Cuisine")
-    private String cuisine;
-
     @Column(name="Overall_Score")
     private String overallScore;
 
