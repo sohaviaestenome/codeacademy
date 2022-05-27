0745f781c90702577e32385a7d40c28be10b0685
pacs <pacs@outlook.pt>
2022-05-27 23:54:02 +0100

    finished fixing database bootstrapping - problem with sql code

--- Restaurant/src/main/resources/data.sql
+++ Restaurant/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into RESTAURANT (id, name, address, overall_score, egg_score) values (hibernate_sequence.nextval, 'Bareburger', '313 W 57th St', 'New York', 'NY', '10019', '212-685-2273', 'https://www.bareburger.com/', 5, 5);
-insert into RESTAURANT (id, name, address,  overall_score, peanut_score) values (hibernate_sequence.nextval, 'Agave Restaurant', '242 Boulevard SE', 'Atlanta', 'GA', '30312', '404-588-0006', 5, 5);
-insert into RESTAURANT (id, name, address, overall_score, dairy_score) values (hibernate_sequence.nextval, 'Erin McKenna''s Bakery NYC', '236 North Larchmont Blvd', 'Los Angeles', 'CA', '90004', 5, 5);
-insert into RESTAURANT (id, name, address, overall_score, peanut_score) values (hibernate_sequence.nextval, 'El Rancho Viejo', '19 W Main St', 'Battle Ground', 'WA', '98604', '360-687-0515', 5, 5);
-insert into RESTAURANT (id, name, address, overall_score, egg_score) values (hibernate_sequence.nextval, 'Hawthorne Fish House', '4343 SE Hawthorne Blvd', 'Portland', 'OR', '97215', 'http://corbettfishhouse.com/', 5, 5);
+insert into RESTAURANT (id, name, address, overall_score, egg_score) values (1, 'Bareburger',  '97217', 5, 5);
+insert into RESTAURANT (id, name, address,  overall_score, peanut_score) values (2, 'Agave Restaurant','97216', 5, 5);
+insert into RESTAURANT (id, name, address, overall_score, dairy_score) values (3, 'Erin McKenna''s Bakery NYC', '90004', 5, 5);
+insert into RESTAURANT (id, name, address, overall_score, peanut_score) values (4,'El Rancho Viejo', '98604', 5, 5);
+insert into RESTAURANT (id, name, address, overall_score, egg_score) values (5, 'Hawthorne Fish House', '97215', 5, 5);
