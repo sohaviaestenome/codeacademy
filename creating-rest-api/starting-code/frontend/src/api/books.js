e319dc4b7834a59c057e9ac22854d50bdff4fe52
pacs <pacs@outlook.pt>
2022-05-30 17:26:22 +0100

    finished

--- creating-rest-api/starting-code/frontend/src/api/books.js
+++ creating-rest-api/starting-code/frontend/src/api/books.js
@@ -1,9 +1,50 @@
-// TODO: Import API_ENDPOINT
+import { API_ENDPOINT } from "./index";
 
-// TODO: Create the addNewBook function that takes in newTitle, newStart, and newEnd as arguments. Inside the function, create a POST request for the new book. Store the response as a JSON in a variable called newBook and return it at the end of the function.
+export const getBooks = async () => {
+  const response = await fetch(`${API_ENDPOINT}/books`);
+  const books = await response.json();
 
-// TODO: Create the getBooks function that retrieves all of the books that have been saved to the back-end API
+  return books;
+};
 
-// TODO: Create the updateBook function that takes the arguments id, newTitle, newStart, newEnd. Inside of the function, create a PUT request for the specified book to be updated. Return the status of the response at the end of the function.
+export const addNewBook = async (newTitle, newStart, newEnd) => {
+  const response = await fetch(`${API_ENDPOINT}/books`, {
+    method: "POST",
+    body: JSON.stringify({
+      title: newTitle,
+      start: newStart,
+      end: newEnd
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-// TODO: Create the deleteBook function that takes the id of the book to be deleted as an argument. Inside of the function, create a DELETE request for the specified book to be deleted. Return the status of the response at the end of the function.
+  const newBook = await response.json();
+
+  return newBook;
+};
+
+export const deleteBook = async (id) => {
+  const response = await fetch(`${API_ENDPOINT}/books/${id}`, {
+    method: "DELETE",
+  });
+
+  return response.status;
+};
+
+export const updateBook = async (id, newTitle, newStart, newEnd) => {
+  const response = await fetch(`${API_ENDPOINT}/books/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      newTitle,
+      newStart,
+      newEnd
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  return response.status;
+};
