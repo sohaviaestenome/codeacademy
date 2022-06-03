64749cdc1853989ed996a1a0cd30dcb912234a30
pacs <pacs@outlook.pt>
2022-06-03 16:55:28 +0100

    learning react router

---
+++ react-router-lesson-starter/src/app/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import About from "../components/About";
+import SignUp from "../components/SignUp";
+import Articles from "../components/Articles";
+import Article from "../components/Article";
+import Categories from "../components/Categories";
+import Author from "../components/Author";
+import Profile from "../components/Profile";
+import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom';
+
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <Router>
+      <Header />
+      <main>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/sign-up">
+          <SignUp />
+        </Route>
+        <Route path="/articles">
+          <Articles />
+        </Route>
+        <Route path="/categories">
+          <Categories />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
