8bcdfa692436293caf4c76171a0956f1f85a7a19
pacs <pacs@outlook.pt>
2022-06-15 15:55:34 +0100

    routes

---
+++ e-commerce/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}...`)
+} )
+
