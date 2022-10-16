013e33ca52a0c4ada497d6efbe71e322d8d4c88d
pacs <pacs@outlook.pt>
2022-10-16 17:34:54 +0100

    finished but still with bugs

--- NODEJS/websockets/hot_potato/server.js
+++ NODEJS/websockets/hot_potato/server.js
@@ -44,9 +44,19 @@ const wsServer = new WebSocket.Server({server});
 wsServer.on('connection', (socket) => {
   console.log('A new client has joined the server');
   socket.on('message', (data) => {
-    console.log(data);
+    console.log(JSON.stringify(data));
+
+    const {type, payload} = JSON.parse(data);
+
+    switch(type) {
+      case CLIENT.MESSAGE.NEW_USER:
+        handleNewUser(socket);
+        break;
+    }
   })
 });
+
+
 // TODO: Define the socket 'message' handler
   // 'NEW_USER' => handleNewUser(socket)
   // 'PASS_POTATO' => passThePotatoTo(newPotatoHolderIndex)
@@ -57,13 +67,23 @@ wsServer.on('connection', (socket) => {
 ///////////////////////////////////////////////
 
 // TODO: Implement the broadcast pattern
-
+const broadcast = (data, socketToOmit) => {
+  wsServer.clients.forEach(connectedSocket => {
+    if (connectedSocket.readyState === WebSocket.OPEN && connectedSocket !== socketToOmit) {
+      connectedSocket.send(JSON.stringify(data));  
+    }
+  });
+}
 
 function handleNewUser(socket) {
   // Until there are 4 players in the game....
   if (nextPlayerIndex < 4) {
     // TODO: Send PLAYER_ASSIGNMENT to the socket with a clientPlayerIndex
-    
+    socket.send(JSON.stringify(
+      {
+        type: SERVER.MESSAGE.PLAYER_ASSIGNMENT,
+        payload: {clientPlayerIndex: nextPlayerIndex}
+      }))
     
     // Then, increment the number of players in the game
     nextPlayerIndex++;
@@ -82,7 +102,7 @@ function handleNewUser(socket) {
   // If 4 players are already in the game...
   else {
     // TODO: Send GAME_FULL to the socket
-    
+    socket.send(JSON.stringify({type: SERVER.MESSAGE.GAME_FULL}));
 
   }
 }
@@ -90,18 +110,27 @@ function handleNewUser(socket) {
 
 function passThePotatoTo(newPotatoHolderIndex) {
   // TODO: Broadcast a NEW_POTATO_HOLDER message with the newPotatoHolderIndex
-  
+  const data = {
+    type: SERVER.BROADCAST.NEW_POTATO_HOLDER,
+    payload: { newPotatoHolderIndex }
+  } 
+  broadcast(JSON.stringify(data));
 }
 
 function startTimer() {
   // Set the clock to start at MAX_TIME (30)
   let clockValue = MAX_TIME;
+
+  let message = {
+    type: SERVER.BROADCAST.COUNTDOWN,
+    payload: { clockValue }
+  }
   
   // Start the clock ticking
   const interval = setInterval(() => {
     if (clockValue > 0) {
       // TODO: broadcast 'COUNTDOWN' with the clockValue
-      
+      broadcast(message);
 
       // decrement until the clockValue reaches 0
       clockValue--;
@@ -113,7 +142,7 @@ function startTimer() {
       nextPlayerIndex = 0; // reset the players index
       
       // TODO: Broadcast 'GAME_OVER'
-   
+      broadcast({type: SERVER.BROADCAST.GAME_OVER})
     }
   }, 1000);
 }
