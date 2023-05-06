85431943023a4a000c76629423d78d2f4a92f628
pacs <pacs@outlook.pt>
2023-05-06 01:20:21 +0100

    remodeling

---
+++ XCode/CodeHistory/CodeHistory/CodeHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeHistoryApp.swift
+//  CodeHistory
+//
+//  Created by Pedro Silva on 06/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CodeHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XCode/CodeHistory/CodeHistory/Models/Question.swift
@@ -0,0 +1,16 @@
+//
+//  Question.swift
+//  CodeHistory
+//
+//  Created by Pedro Silva on 06/05/2023.
+//
+
+import Foundation
+
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+}
+
+
