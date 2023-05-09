2e57954114a913ebe5f209b93fa5f8d778f58d96
pacs <pacs@outlook.pt>
2023-05-09 18:33:24 +0100

    just missing final screen

--- XCode/CodeHistory/CodeHistory/Views/QuestionView.swift
+++ XCode/CodeHistory/CodeHistory/Views/QuestionView.swift
@@ -8,31 +8,40 @@
 import SwiftUI
 
 struct QuestionView: View {
- 
-  let question: Question
- 
-  var body: some View {
-    VStack {
-      Text(question.questionText)
-        .font(.largeTitle)
-        .bold()
-        .multilineTextAlignment(.leading)
-      Spacer()
-      HStack {
-        ForEach(0..<question.possibleAnswers.count) { answerIndex in
-          Button(action: {
-            print("Tapped on option with the text: \(question.possibleAnswers[answerIndex])")
-          }) {
-            ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
-          }
+    @EnvironmentObject var viewModel: GameViewModel
+    let question: Question
+    
+    var body: some View {
+        VStack {
+            Text(question.questionText)
+                .font(.largeTitle)
+                .bold()
+                .multilineTextAlignment(.leading)
+            Spacer()
+            HStack {
+                ForEach(0..<question.possibleAnswers.count) { index in
+                    Button {
+                        viewModel.makeGuess(atIndex: index)
+                        print("Tapped on option with the text: \(question.possibleAnswers[index])")
+                    } label: {
+                        ChoiceTextView(choiceText: question.possibleAnswers[index])
+                            .background(viewModel.color(forOptionIndex: index))
+                    }
+                    .disabled(viewModel.guessWasMade)
+                }
+            }
+            if viewModel.guessWasMade {
+                Button(action: { viewModel.displayNextScreen() }) {
+                    BottomTextView(str: "Next")
+                }
+            }
         }
-      }
     }
-  }
 }
 
 struct QuestionView_Previews: PreviewProvider {
     static var previews: some View {
-      QuestionView(question: Game().currentQuestion)
+        QuestionView(question: Game().currentQuestion)
+            .environmentObject(GameViewModel())
     }
 }
