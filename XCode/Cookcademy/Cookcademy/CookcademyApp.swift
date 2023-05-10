bc066cd105a1a29dd849f9a0e050c10e2f25c5e5
pacs <pacs@outlook.pt>
2023-05-10 12:34:07 +0100

    started last project...cookcademy

---
+++ XCode/Cookcademy/Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Pedro Silva on 10/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
