bfb73d3c620c5b56b92aa877ffaf992797002a71
pacs <pacs@outlook.pt>
2023-05-11 00:43:14 +0100

    forms and pickers exercise

---
+++ XCode/Contact Form/Contact Form/Contact_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contact_FormApp.swift
+//  Contact Form
+//
+//  Created by Pedro Silva on 11/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Contact_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
