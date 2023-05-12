85c3cb20b56514f28e81ccaf138f5533f4296501
pacs <pacs@outlook.pt>
2023-05-12 16:54:36 +0100

    testing project

---
+++ XCode/Testing/Testing/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by Pedro Silva on 12/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
