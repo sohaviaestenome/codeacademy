e7a8a43022735ba7de4477614b3963f31d422ef6
pacs <pacs@outlook.pt>
2023-05-12 12:35:34 +0100

    created new project to test concurrency and parallelism..

---
+++ XCode/RandomUsers/RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Pedro Silva on 12/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
