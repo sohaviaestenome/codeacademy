1eb56635d4f0112d53435a4a95bd32b717d65f24
pacs <pacs@outlook.pt>
2023-05-12 23:29:11 +0100

    created final project

---
+++ XCode/Socialcademy/Socialcademy/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Socialcademy
+//
+//  Created by Pedro Silva on 12/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XCode/Socialcademy/Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by Pedro Silva on 12/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SocialcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
